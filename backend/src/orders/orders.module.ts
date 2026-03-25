6c2abcdba3111920d9b38dc631a9839bbb8d6a28
PPWAURK <ppworkhardspace@gmail.com>
2026-03-25 14:40:38 +0100

    refactor: extract order document generation service

--- backend/src/orders/orders.module.ts
+++ backend/src/orders/orders.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
+import { OrdersDocumentService } from './orders-document.service';
 import { OrdersService } from './orders.service';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersDocumentService],
   exports: [OrdersService],
 })
 export class OrdersModule {}
